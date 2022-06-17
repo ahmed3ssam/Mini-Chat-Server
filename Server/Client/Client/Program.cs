76dab69290ee79595f4e226f6fb810481bc405bf
Ahmed Essam <96630321+ahmed3ssam@users.noreply.github.com>
2022-06-17 08:38:01 +0200

    Add files via upload

---
+++ Server/Client/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Networks_Lab_assignment
+{
+    class Client
+    {
+
+        static void Main(string[] args)
+        {
+            IPAddress host = IPAddress.Parse("127.0.0.1");
+
+            IPEndPoint hostEndpoint = new IPEndPoint(host, 8000);
+
+            Socket clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+            clientSock.Connect(hostEndpoint);
+
+            byte[] receivedData = new byte[1024];
+            int receivedBytesLen = clientSock.Receive(receivedData);
+            int fileNameLength = BitConverter.ToInt32(receivedData, 0);
+            String fileName = Encoding.ASCII.GetString(receivedData, 4, fileNameLength);
+            BinaryWriter binaryWriter = new BinaryWriter(File.Open(fileName, FileMode.Create));
+            binaryWriter.Write(receivedData, 4 + fileNameLength, receivedBytesLen);
+           binaryWriter.Close();
+            Console.Write(">>File Name : ");
+            Console.WriteLine(fileName);
+            Console.WriteLine(">>File Has been Recieved, Saved at 'Client Debug' Folder........");
+            Console.ReadLine();
+            clientSock.Shutdown(SocketShutdown.Both);
+            clientSock.Close();
+
+        }
+
+    }
+}
---
+++ Server/Server/Server/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace Networks_Lab_assignment
+{
+    class Server
+    {
+        static void Main()
+        {
+            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Any, 8000);
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            sock.Bind(ipEnd);
+
+            sock.Listen(100);
+
+            Console.WriteLine("Ahmed Essam Sayed Mohamed\nCyber Security Student\n");
+
+            while (true)
+            {
+                Socket clientSock = sock.Accept();
+                Console.WriteLine(">> Client Connection is on");
+
+                Console.Write(">> Enter File Name, File type(.txt / .exe): ");
+                string fileName = Console.ReadLine();
+
+                Console.WriteLine(">> Sending File....");
+                byte[] wholeFile = new byte[1000];
+                byte[] fileNameByte = Encoding.ASCII.GetBytes(fileName);
+                byte[] fileNameLength = BitConverter.GetBytes(fileNameByte.Length);
+                byte[] fileData = File.ReadAllBytes(fileName);
+
+                fileNameLength.CopyTo(wholeFile, 0);
+                fileNameByte.CopyTo(wholeFile, 4);
+                fileData.CopyTo(wholeFile, 4 + fileNameByte.Length);
+
+                clientSock.Send(wholeFile);
+                Console.WriteLine(">> File Sent....");
+
+                clientSock.Shutdown(SocketShutdown.Both);
+                clientSock.Close();
+            }
+
+        }
+    }
+}
